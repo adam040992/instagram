cf77c36cfc86e4f38b5fedeeb5e40c1f12f331b5
adam040992 <adam040992@gmail.com>
2022-11-06 13:44:51 +0100

    Add: Header-Done

--- Components/Header.js
+++ Components/Header.js
@@ -12,7 +12,7 @@ import { HomeIcon } from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <div>
+    <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
         <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
             {/* Left */}
             <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
@@ -49,10 +49,15 @@ const Header = () => {
             <div className='flex items-center justify-end space-x-4'>
                 <MenuIcon className='h-6 md:hidden cursor-pointer' />
                 <HomeIcon className='navBtn' />
-                <PaperAirplaneIcon className='navBtn' />
+                <div className='relative navBtn'>
+                    <PaperAirplaneIcon className='navBtn' />
+                    <div className='absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white'>3</div>
+                </div>
                 <PlusCircleIcon className='navBtn' />
                 <UserGroupIcon className='navBtn' />
                 <HeartIcon className='navBtn' />
+
+                <img src='https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745' alt='profile pic' className='h-10 rounded-full cursor-pointer' />
             </div>
         </div>
     </div>
